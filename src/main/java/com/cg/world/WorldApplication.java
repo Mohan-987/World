9598b11fddc47bf32a22be24ba909cb7c889e201
Mohan-987 <poluri-mani.mohan@capgemini.com>
2023-07-14 09:11:04 +0530

    Second commit

--- src/main/java/com/cg/world/WorldApplication.java
+++ src/main/java/com/cg/world/WorldApplication.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class WorldApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorldApplication.class, args);
 	}
--- src/main/java/com/cg/world/repository/CountryLanguageRepository.java
+++ src/main/java/com/cg/world/repository/CountryLanguageRepository.java
@@ -1,12 +1,10 @@
 package com.cg.world.repository;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import com.cg.world.entity.enums.IsOfficial;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -21,7 +19,4 @@ public interface CountryLanguageRepository extends JpaRepository<CountryLanguage
 	List<CountryLanguage> findAllByIsOfficial(IsOfficial isOfficial);
 	List<CountryLanguage> findByCountryCodeAndIsOfficial(String countryCode, IsOfficial isOfficial);
 	Optional<CountryLanguage> findByCountryCodeAndLanguage(String countryCode, String language);
-	@Modifying
-	@Query("UPDATE CountryLanguage cl SET cl.percentage = :percentage WHERE cl.country.code = :countryCode AND cl.language = :language")
-	void updatePercentageByCountryCodeAndLanguage(String countryCode, String language,BigDecimal percentage);
 }
